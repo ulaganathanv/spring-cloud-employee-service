2a997fa2583df6f0f06969e5aea3c978c18ac645
ulag-india <ulag.india@gmail.com>
2018-07-07 23:34:00 +0530

    Base Version

---
+++ src/main/java/com/mypackage/sample/springcloudemployeeservice/SpringCloudEmployeeServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mypackage.sample.springcloudemployeeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEmployeeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEmployeeServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mypackage/sample/springcloudemployeeservice/SpringCloudEmployeeServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mypackage.sample.springcloudemployeeservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudEmployeeServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
